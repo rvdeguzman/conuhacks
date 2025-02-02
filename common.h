f7dc15c324ee94cd545e8fabb1669c1f1065775f
rv <rvdeguzman@protonmail.com>
2025-02-02 00:38:12 -0500

    commons updated

--- common.h
+++ common.h
@@ -3,21 +3,28 @@
 #include <cstdint>
 #include <enet/enet.h>
 
+const int MAP_WIDTH = 8;
+const int MAP_HEIGHT = 8;
+
+const int worldMap[MAP_WIDTH][MAP_HEIGHT] = {
+    {4, 4, 4, 4, 4, 4, 4, 4}, {4, 0, 0, 0, 0, 0, 0, 4},
+    {4, 0, 2, 0, 0, 2, 0, 4}, {4, 0, 0, 0, 0, 0, 0, 1},
+    {4, 0, 2, 0, 0, 2, 0, 4}, {4, 0, 2, 0, 0, 2, 0, 4},
+    {4, 0, 0, 0, 0, 0, 0, 4}, {4, 3, 3, 1, 1, 3, 3, 4}};
+
 enum PacketType { PLAYER_POSITION = 1, PLAYER_INPUT };
 
 struct PlayerState {
-    double posX;
-    double posY;
-    double dirX;
-    double dirY;
-    double planeX;
-    double planeY;
-    bool isMoving;
+  double posX;
+  double posY;
+  double dirX;
+  double dirY;
+  double planeX;
+  double planeY;
+  bool isMoving;
 
-    PlayerState() : 
-        posX(2.0), posY(2.0),
-        dirX(-1.0), dirY(0.0),
-        planeX(0.0), planeY(0.66),
+  PlayerState()
+      : posX(2.0), posY(2.0), dirX(-1.0), dirY(0.0), planeX(0.0), planeY(0.66),
         isMoving(false) {}
 };
 
@@ -39,7 +46,13 @@ struct ShotAttemptPacket {
   double shooterDirX;
   double shooterDirY;
 };
-
+struct ShotVisualizationPacket {
+  double startX;
+  double startY;
+  double endX;
+  double endY;
+  double duration;
+};
 struct HitNotificationPacket {
   size_t shooterID;
   size_t targetID;
